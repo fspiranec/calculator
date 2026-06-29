7438bdcf29725a8c65b3635267d8f7410239d1bf
fspiranec <85845602+fspiranec@users.noreply.github.com>
2026-06-29 11:48:52 +0200

    Type Supabase server cookie setter

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,12 +1,18 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
+type CookieToSet = {
+  name: string;
+  value: string;
+  options?: Parameters<ReturnType<typeof cookies>['set']>[2];
+};
+
 export const createSupabaseServerClient = () => {
   const cookieStore = cookies();
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
       getAll: () => cookieStore.getAll(),
-      setAll: (cookiesToSet) => cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options)),
+      setAll: (cookiesToSet: CookieToSet[]) => cookiesToSet.forEach(({ name, value, options }: CookieToSet) => cookieStore.set(name, value, options)),
     },
   });
 };
